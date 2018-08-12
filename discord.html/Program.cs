fb3c14295ac15be7aac576983da0fe1fbd0a844c
3wayHimself <valvevacbanned@gmail.com>
2018-08-11 17:26:04 -0700

    God save the queen.

---
+++ discord.html/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Mime;
+
+namespace discord.html
+{
+    using System;
+    using System.Text;
+    using System.Windows.Forms;
+
+    class Program
+    {
+        private static string version = "1.0.0";
+
+        private static void ShowHelp()
+        {
+            Console.WriteLine($"Discord.html Bot Runner (Command Line Interface) v{version}");
+            Console.WriteLine();
+            Console.WriteLine("[--bot]          HTML file containing bot logic.");
+            Console.WriteLine();
+            Console.WriteLine("# Example Usage:");
+            Console.WriteLine("   discord.html.exe --bot=\"C:\\discord.html\\Bot\\bot.html\"");
+        }
+
+        static void Main(string[] args)
+        {
+            // Set encoding because I'm not shit at what I do.
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // Set console title.
+            Console.Title = $"Discord.html Bot Runner (Command Line Interface) v{version}";
+
+            if (args.Length >= 0)
+            {
+                ShowHelp();
+            }
+
+            // Recursively cycle through arguments.
+            foreach (var cArg in args)
+            {
+                if (cArg[0] == '-')
+                {
+                    if (cArg == "--bot")
+                    {
+                        // TODO:
+                        // Implement
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ specs.md
@@ -0,0 +1,12 @@
+# Discord.html Specification
+
+"blah blah not real HTML blah"
+I don't care. Save it for the imaginary friend that does.
+
+## methods
+
+- discord.html
+	- Program
+		- Main
+	- Cleaner
+		- minify
